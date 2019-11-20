17ba99da213b039bf4bc9034233ac24beb0f5b58
Angel Lopez Manriquez <sigma271@outlook.com>
2019-11-20 22:21:39 +0100

    Update lab5trigger.sql

--- lab5trigger.sql
+++ lab5trigger.sql
@@ -1,7 +1,3 @@
-
-
--- 1 Crear tablas
-
 DROP TABLE departamento17;
 
 CREATE TABLE departamento17 (
@@ -19,77 +15,58 @@ CREATE TABLE empleado17 (
 );
 
 
-INSERT INTO departamento17(nombre, presupuestopersonal) VALUES ('nombre1', 0);
-INSERT INTO departamento17(nombre, presupuestopersonal) VALUES ('nombre2', 0);
-INSERT INTO departamento17(nombre, presupuestopersonal) VALUES ('nombre3', 0);
-INSERT INTO departamento17(nombre, presupuestopersonal) VALUES ('nombre4', 0);
+INSERT INTO departamento17(nombre, presupuestopersonal) VALUES ('depto1', 0);
+INSERT INTO departamento17(nombre, presupuestopersonal) VALUES ('depto2', 0);
+INSERT INTO departamento17(nombre, presupuestopersonal) VALUES ('depto3', 0);
+INSERT INTO departamento17(nombre, presupuestopersonal) VALUES ('depto4', 0);
 
 SELECT * FROM departamento17;
+SELECT * FROM empleado17;
 
--- 1 Triggers =================================================================
--- Crear un disparador que controle que el atributo presupuestoPersonal del
--- departamento se calcula como la suma de los salarios de los empleados que trabajan en
--- dicho departamento (sólo para los casos de inserción de nuevos empleados y de
--- modificación del sueldo de los ya existentes). Verificar el funcionamiento del trigger 
 
-CREATE OR REPLACE TRIGGER uno 
-    AFTER INSERT OR UPDATE OF salario ON empleado17
-    FOR EACH ROW 
-BEGIN 
+-- 1
+
+CREATE OR REPLACE TRIGGER t1 
+    AFTER INSERT OR UPDATE OF salario ON empleado17 
+    FOR EACH ROW
+BEGIN
     IF INSERTING THEN 
-        UPDATE departamento17
+        UPDATE departamento17 
             SET presupuestopersonal = presupuestopersonal + :new.salario
-            WHERE nombre = :new.sudpto ;
+            WHERE nombre = :new.sudpto;
     ELSE 
-        UPDATE departamento17
+        UPDATE departamento17 
             SET presupuestopersonal = presupuestopersonal + :new.salario - :old.salario
-            WHERE departamento17.nombre = :new.sudpto ;
+            WHERE nombre = :new.sudpto;
     END IF;
 END;
+/
 
-UPDATE departamento17 set presupuestopersonal = 0;
-SELECT * FROM departamento17;
-SELECT * FROM empleado17;
 
--- '49578508F'
-INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 0, 1000, 'nombre1' );
-INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 1, 500, 'nombre1' );
-SELECT * FROM empleado17;
+-- stir fry
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 0, 1000, 'depto1' );
 SELECT * FROM departamento17;
 
-UPDATE empleado17 SET salario = 400 WHERE dni = 0;
+UPDATE empleado17 SET salario = salario - 200 WHERE dni = 0;
 SELECT * FROM departamento17;
 
--- 2 
--- Crear un disparador que al introducir el empleado con DNI 66, introducir su nuevo
--- departamento en la tabla DEPARTAMENTO y que dicha tupla tenga como
--- Presupuestopersonal el valor de 100000 euros. Usar la opción REFERENCING
 
--- CREATE TABLE departamento17 (
---     nombre VARCHAR2(20),
---     presupuestopersonal NUMBER, 
---     PRIMARY KEY(nombre) 
--- );
-
--- CREATE TABLE empleado17(
---     dni NUMBER PRIMARY KEY,
---     salario NUMBER,
---     sudpto VARCHAR2(20) REFERENCES departamento17(nombre)
--- );
-
-
-CREATE OR REPLACE TRIGGER dos 
+-- 2
+CREATE OR REPLACE TRIGGER t2 
     BEFORE INSERT ON empleado17
     REFERENCING NEW AS nuevo
     FOR EACH ROW 
     WHEN (nuevo.dni = 66)
 BEGIN 
-    INSERT INTO departamento17 ( nombre , presupuestopersonal ) VALUES (:nuevo.sudpto, 100000);
+    INSERT INTO departamento17 (nombre, presupuestopersonal) 
+       VALUES (:nuevo.sudpto, 100000);
 END;
+/
 
-INSERT INTO empleado17(dni, salario, sudpto) VALUES (66, 100, 'Ibai');
-SELECT * FROM empleado17;  
-SELECT * FROM departamento17;  -- 100000 + 100
+SELECT * FROM departamento17;
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 66, 100, 'Jefe' );
+SELECT * FROM departamento17;
+
 
 -- 3
 -- Realizar un seguimiento de los cambios en el salario de los empleados (inserciones y
@@ -103,95 +80,107 @@ CREATE TABLE CambioSalario_log (
     salariodespues number
 ); 
 
--- CREATE TABLE empleado17(
---     dni NUMBER PRIMARY KEY,
---     salario NUMBER,
---     sudpto VARCHAR2(20) REFERENCES departamento17(nombre)
--- );
-
 CREATE OR REPLACE TRIGGER t3 
-    AFTER INSERT OR UPDATE OF salario ON empleado17
+    AFTER INSERT OR UPDATE OF salario ON empleado17 
     FOR EACH ROW 
-BEGIN
+BEGIN 
     IF INSERTING THEN 
-        INSERT INTO CambioSalario_log (
-            quien , 
-            cuando , 
-            dniEmpleado , 
-            salarioAntes , 
-            salariodespues ) 
-        VALUES ( USER, SYSDATE, :new.dni, 0, :new.salario) ;
+        INSERT INTO CambioSalario_log (quien, cuando, dniEmpleado, salarioAntes, salariodespues)
+            VALUES (USER, SYSDATE, :NEW.dni, NULL, :NEW.salario);
     ELSE 
-        INSERT INTO CambioSalario_log (
-            quien , 
-            cuando , 
-            dniEmpleado , 
-            salarioAntes , 
-            salariodespues ) 
-        VALUES ( USER, SYSDATE, :new.dni, :old.salario, :new.salario) ;
+        INSERT INTO CambioSalario_log (quien, cuando, dniEmpleado, salarioAntes, salariodespues)
+            VALUES (USER, SYSDATE, :NEW.dni, :OLD.salario, :NEW.salario);
     END IF;
 END;
-
-SELECT * FROM empleado17;
-
-INSERT INTO empleado17(dni, salario, sudpto) VALUES (2, 200, 'nombre2');
-INSERT INTO empleado17(dni, salario, sudpto) VALUES (3, 300, 'nombre3');
+/ 
 
 SELECT * FROM CambioSalario_log;
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 1, 2000, 'depto1' );
+SELECT * FROM CambioSalario_log;
 
-UPDATE empleado17 SET salario = salario + 100 WHERE dni = 2;
-UPDATE empleado17 SET salario = salario - 100 WHERE dni = 3;
+UPDATE empleado17 SET salario = salario - 1000;
+SELECT * FROM CambioSalario_log;
 
+UPDATE empleado17 SET salario = salario - 1000;
 SELECT * FROM CambioSalario_log;
 
 
 -- 4
--- Evitar que el salario sea actualizado durante el mes de Noviembre
--- MON- Abreviatura de MONTH
--- Códigos de error entre -20000 y -20999 
-
--- CREATE TABLE empleado17(
---     dni NUMBER PRIMARY KEY,
---     salario NUMBER,
---     sudpto VARCHAR2(20) REFERENCES departamento17(nombre)
--- );
-
-CREATE OR REPLACE TRIGGER t4 
+CREATE OR REPLACE TRIGGER t4
     BEFORE UPDATE OF salario ON empleado17
+    FOR EACH ROW 
 BEGIN 
+    -- IF MON = 'NOV' THEN 
     IF SYSDATE LIKE '__/11/__' THEN 
-        RAISE_APPLICATION_ERROR(-20000, 'No se puede actualizar el salario en noviembre.');
-    END IF;
-END;
-
-SELECT * FROM empleado17;
-UPDATE empleado17 SET salario = salario + 100 WHERE dni = 2;  -- Exception raised
-
-
--- Trigger 5. Restringir el número máximo de empleados de un departamento a 3 
-
-CREATE OR REPLACE TRIGGER t5 
-    BEFORE INSERT ON empleado17
-    FOR EACH ROW
-    DECLARE people_count INTEGER;
-BEGIN 
-    SELECT COUNT(*) INTO people_count 
-        FROM empleado17 e, departamento17 d
-        WHERE d.nombre = e.sudpto AND e.sudpto = :NEW.sudpto;
-    IF people_count = 3 THEN 
-        RAISE_APPLICATION_ERROR(-20999, 'No se puede tener mas de tres empleados');
+        RAISE_APPLICATION_ERROR(-20000, 'Cannot update salary on november');
     END IF;
 END; 
+/
 
-SELECT COUNT(*) 
-    FROM empleado17 e, departamento17 d
-    WHERE d.nombre = e.sudpto AND e.sudpto = 'nombre2';
+UPDATE empleado17 SET salario = salario + 1000;
+-- exception raised, OK
 
-SELECT * FROM empleado17;
-INSERT INTO empleado17(DNI, salario, sudpto) VALUES (3, 400, 'nombre2' );
-INSERT INTO empleado17(DNI, salario, sudpto) VALUES (4, 400, 'nombre2' );
-INSERT INTO empleado17(DNI, salario, sudpto) VALUES (5, 400, 'nombre2' );
+-- 5
+-- higher we go
+-- INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 66, 100, 'Jefe' );
 
-INSERT INTO empleado17(DNI, salario, sudpto) VALUES (6, 400, 'nombre2' ); -- Exception raised
+SELECT * FROM departamento17;
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 2, 100, 'Jefe' );
+SELECT * FROM empleado17 WHERE sudpto = 'Jefe';
 
 
+CREATE OR REPLACE TRIGGER t5 
+    BEFORE INSERT ON empleado17 
+    FOR EACH ROW 
+    DECLARE e_count INTEGER;
+BEGIN 
+    -- SELECT COUNT(*) 
+    -- SELECT * 
+    SELECT COUNT(*) INTO e_count FROM empleado17 WHERE sudpto = :new.sudpto; 
+    IF e_count = 3 THEN 
+        RAISE_APPLICATION_ERROR(-20000, 'There are enough');
+    END IF;
+END;
+/
+
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 3, 100, 'Jefe' );
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 4, 100, 'Jefe' );
+-- Error
+
+
+-- 6
+ALTER TABLE empleado17 
+    ADD CONSTRAINT c_salario CHECK (salario < 1000000);
+SELECT * FROM CambioSalario_log;
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 5, 9999999999, 'Jefe' );
+-- Excepcion reportada por gatillo
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 5, 9999999999, 'depto2' );
+-- Excepcion reportada por restriccion de integridad
+
+
+-- 7
+ALTER TRIGGER t5 DISABLE;
+INSERT INTO empleado17( dni , salario , sudpto ) VALUES ( 5, 9999999999, 'Jefe' );
+-- excepcion, violacion de regla de integridad
+ALTER TRIGGER t5 ENABLE;
+
+
+-- 8
+SELECT trigger_type, triggering_event, table_name
+    FROM user_triggers 
+    WHERE trigger_name = 't5';
+
+SELECT trigger_name
+    FROM user_triggers 
+    WHERE user = 'system';
+-- en ambos casos no se selecciono ninguna fila
+
+
+SELECT trigger_type, triggering_event, table_name
+    FROM user_triggers ;
+
+SELECT trigger_name
+    FROM user_triggers ;
+
+SELECT trigger_name, trigger_type, triggering_event, table_name
+    FROM user_triggers ;
