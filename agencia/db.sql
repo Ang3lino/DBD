e1d8dd865744a4017ee181c50d86991ec856674b
Angel Lopez Manriquez <sigma271@outlook.com>
2019-11-24 14:50:53 +0100

    Add files via upload

--- agencia/db.sql
+++ agencia/db.sql
@@ -93,11 +93,14 @@ BEGIN
 END; 
 /
 
+
 CREATE TABLE viaje (
     fecha_inicio DATE NOT NULL,
     descripcion VARCHAR2(256),
     nombre VARCHAR2(64),
     precio NUMBER,
+    reservados NUMBER DEFAULT 0,
+    lugares NUMBER NOT NULL,
     CONSTRAINT viaje_pk PRIMARY KEY(fecha_inicio)
 );
 
--- agencia/restricciones.sql
+++ agencia/restricciones.sql
@@ -30,7 +30,7 @@ CREATE VIEW v_estudiante_nota_media AS
     FROM persona p 
         JOIN estudiante e ON p.id = e.persona_id
         JOIN materia m  ON e.persona_id = m.estudiante_id 
-    GROUP BY p.id;
+    GROUP BY p.id, p.nombre, p.primer_apellido, p.segundo_apellido;
 
 -- visualizar la cantidad invertida por los clientes en el viaje con su nombre
 CREATE VIEW v_cliente_inversion AS 
@@ -39,7 +39,7 @@ CREATE VIEW v_cliente_inversion AS
     FROM persona p 
         JOIN cliente c ON p.id = c.id 
         JOIN compra_viaje cv ON c.id = cv.cliente_id 
-    GROUP BY p.id 
+    GROUP BY p.id, c.nickname
     ORDER BY total DESC;    
 
 -- mostrar todas las habitaciones reservadas para el dia de hoy
@@ -47,7 +47,7 @@ CREATE VIEW v_habitaciones_hoy AS
     SELECT 
         h.ubicacion, v.nombre, hr.planta, hr.numero, hr.cant_personas 
     FROM habitacion_reservada hr 
-        JOIN hospedaje h ON (hr.ubicacion, hr.fecha) = (h.ubicacion, h.fecha) 
+        JOIN hospedaje h ON hr.ubicacion = h.ubicacion AND hr.fecha = h.fecha
         JOIN viaje v ON h.viaje_id = v.fecha_inicio 
     WHERE v.fecha_inicio = SYSDATE;
 
